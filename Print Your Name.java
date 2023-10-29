7b147ec9638103cbfceb3ffb73cd09bd28ac9919
Reyan Shafi <106812655+reyanshafi@users.noreply.github.com>
2023-10-29 23:45:11 +0530

    Create Print Your Name.java

---
+++ Print Your Name.java
@@ -0,0 +1,8 @@
+class hello{
+    public static void main(String arg[]){
+
+        System.out.println("Day on of Java Programming");
+        System.out.println("My name is Reyan");
+       
+    }
+}
